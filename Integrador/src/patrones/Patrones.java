e57b167b7efca15d73af6b33b12fabe6dd34277b
Daniel Arroyo <darroyo@espol.edu.ec>
2018-12-17 23:45:26 -0500

    Agregado Main.

--- Integrador/src/patrones/Patrones.java
+++ Integrador/src/patrones/Patrones.java
@@ -16,6 +16,45 @@ public class Patrones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ComprobantesFactory factory = new ComprobantesFactory();
+
+        ComprobanteElectronico comprobante = factory.getComprobante("FACTURA");
+
+        comprobante.setClaveAcceso("1234567890");
+
+        comprobante.setCodigo(90000);
+
+        ArrayList<String> detalles = new ArrayList<>();
+
+        detalles.add("0991225869");
+
+        detalles.add("daniel0ar");
+
+        comprobante.setDetallesEmisor(detalles);
+
+        LocalDate date = LocalDate.of(2018, Month.DECEMBER, 17);
+
+        comprobante.setFecha(date);
+
+        comprobante.setNombreCliente("Daniel");
+
+        AutorizadorSRI sri = new AutorizadorSRI(new EsquemaOnline());
+
+        sri.autorizar(comprobante);
+
+        Factura factura = (Factura)comprobante;
+
+        factura.setTotal(20.00);
+
+        ArrayList<String> productos = new ArrayList<>();
+
+        productos.add("carro");
+
+        productos.add("mesa");
+
+        factura.setProductos(productos);
+
+        System.out.println(factura.toString());
     }
     
 }
